a06aa3633f00d71973afd9a7794c7b89c6dd70df
Sam Hall <mann.samantha11@gmail.com>
2020-12-05 21:15:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,21 +12,10 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
-
-  if result == emoticons[emoticon][0]
-    emoticon[1]
-  else
-    "Sorry, that emoticon was not found"
-  end
+  
   end
 
 def get_japanese_emoticon(path, emoticon)
   emoticons = load_library(path)
-  result = emoticons["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+  
 end
