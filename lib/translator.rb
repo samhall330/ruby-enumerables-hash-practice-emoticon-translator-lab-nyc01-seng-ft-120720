53bc948828b99bd543183de22821c288f1cc11a6
Sam Hall <mann.samantha11@gmail.com>
2020-12-05 21:15:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,10 +12,10 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
-  
+
   end
 
 def get_japanese_emoticon(path, emoticon)
   emoticons = load_library(path)
-  
+
 end
